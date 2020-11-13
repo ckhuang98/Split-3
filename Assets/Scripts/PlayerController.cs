c233f117654565b2332cb05b3839670601479683
Tennessee Phillips Ward <twarior@gmail.com>
2020-11-13 13:22:09 -0600

    Player can rotate camera with Q and E and perform a basic attack.
    Attacking with an item tagged "Weapon" will cause enemies to change color and then disappear over time.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.ExceptionServices;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Video;
@@ -9,10 +10,18 @@ public class PlayerController : MonoBehaviour {
 
     public float speed;
     private Rigidbody rb;
-    public Camera camera; 
+    public Camera camera;
+    public GameObject weapon;
+    private int lastDirection = 1;
+    private bool attacking = false;
+    private float timer;
+    public float smooth = 1f;
+    private Quaternion targetRotation;
     // Start is called before the first frame update
     void Start() {
         rb = this.GetComponent<Rigidbody>();
+        timer = 0;
+        targetRotation = transform.rotation;
     }
 
     // Update is called once per frame
@@ -22,7 +31,7 @@ public class PlayerController : MonoBehaviour {
         float verticalAxis = Input.GetAxis("Vertical");
 
         
-        Debug.Log(camera);
+        //Debug.Log(camera);
 
         var forward = camera.transform.forward;
         var right = camera.transform.right;
@@ -37,12 +46,38 @@ public class PlayerController : MonoBehaviour {
 
 
         transform.position += (desiredMoveDirection * speed * Time.deltaTime);
-        
+
         //var characterController = this.GetComponent<CharacterController>();
         //characterController.SimpleMove(desiredMoveDirection.normalized *speed );
-       
+        
+
+        if (Input.GetMouseButtonDown(0) && timer >=.5) {
+            attacking = true;
+
+            lastDirection = -1 * lastDirection;
+            timer = 0;
+        }
+        if (attacking && timer <= .5) {
+            weapon.transform.RotateAround(gameObject.transform.position, Vector3.up,
+                    180 * lastDirection * Time.deltaTime);
+        }
+        timer += Time.deltaTime;
+
+
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            targetRotation *= Quaternion.AngleAxis(60, Vector3.up);
+        }
+        else if (Input.GetKeyDown(KeyCode.E)) {
+            targetRotation *= Quaternion.AngleAxis(60, Vector3.down);
+        }
+        transform.rotation = Quaternion.Lerp(transform.rotation, 
+                                            targetRotation, 
+                                            10 * smooth * Time.deltaTime);
     }
 
-    
+    void OnClick(GameObject weapon, GameObject Player) {
+
+        
+    }
 
 }
